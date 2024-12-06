a32b0005040b4d7f30283ae5528f2395db3d32e5
Valerii <valeriy.stronskiy@gmail.com>
2024-12-06 16:13:30 +0200

    Part One

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Advent_of_Code___Day_3
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent_of_Code___Day_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string REGEX_FIND_MULT = @"mul\(\d+,\d+\)";
+            const string INPUT_PATH = @"G:\Visual Studio Projects\Advent of Code - Day 3\input.txt";
+
+            int sum = 0;
+            Regex multRegex = new Regex(REGEX_FIND_MULT);
+            string fileData = File.ReadAllText(INPUT_PATH);
+
+            foreach (Match match in multRegex.Matches(fileData))
+            {
+                string[] operands = match.Value.Split([ '(', ',', ')' ], StringSplitOptions.RemoveEmptyEntries);
+
+                sum += Int32.Parse(operands[1]) * Int32.Parse(operands[2]);
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
