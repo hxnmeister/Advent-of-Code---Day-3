d680ff42befbb48f2e674375a6c2c464ac61943f
Valerii <valeriy.stronskiy@gmail.com>
2024-12-06 15:04:57 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advent_of_Code___Day_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
